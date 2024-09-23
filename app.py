f55ad0c845b2f849c2b53b4656e83d9b32ded3ef
goisbrunoaugusto <brunogois902@gmail.com>
2024-09-23 18:24:34 -0300

    feat: scale up, scale down, status

--- app.py
+++ app.py
@@ -1,64 +1,108 @@
 """Códigos para as funçoes de listar, escalonar, monitorar"""
-from flask import Flask
+from flask import Flask, jsonify, Response, request
 from kubernetes import config, client, watch
 import kubernetes.client
 
 app = Flask(__name__)
 
-
+@app.route("/list", methods=['GET'])
 def get_minikube_pod_info():
     """
     Funçao para listar os pods de todos os namespaces
     """
     config.load_kube_config()
     v1 = client.CoreV1Api()
+    pod_list = []
 
     try:
         pods = v1.list_pod_for_all_namespaces()
 
         for pod in pods.items:
-            pod_name = pod.metadata.name
-            pod_status = pod.status.conditions[-1].type
-            pod_creation_time = pod.metadata.creation_timestamp
+            pod_info = {
+                "pod_name" : pod.metadata.name,
+                "pod_phase" : pod.status.phase,
+                "pod_creation_time" : pod.metadata.creation_timestamp,
+                "pod_namespace" : pod.metadata.namespace,
+                "pod_image" : pod.spec.containers[0].image
+            }
 
-            print(f"\nNode Name: {pod_name}")
-            print(f"Status: {pod_status}")
-            print(f"Creation Time: {pod_creation_time}")
+            pod_list.append(pod_info)
+        return jsonify(pod_list)
 
     except client.exceptions.ApiException as e:
-        print(f"An error occurred while retrieving nodes: {e}")
+        return Response("Erro ao listar pods", status=500)
 
-def create_new_pod():
-    """
-    Funçao para criar um novo pod
-    """
+
+# @app.route("/scale-up", methods=['POST'])
+# def create_new_pod():
+#     """
+#     Funçao para criar um novo pod
+#     """
+#     data = request.get_json()
+
+#     config.load_kube_config()
+#     pod = client.V1Pod(
+#     metadata=client.V1ObjectMeta(name=data.get('pod-name')),
+#     spec=client.V1PodSpec(
+#         containers=[
+#             client.V1Container(
+#                 name=data.get('container-name'),
+#                 image=data.get('image')            )
+#         ]
+#     ),
+#     )
+#     try:
+#         response = client.CoreV1Api().create_namespaced_pod(
+#             body=pod, namespace=data.get('namespace')
+#         )
+#         return Response(status=201)
+    
+#     except client.exceptions.ApiException as e:
+#         return Response("Erro ao criar pod", status=500)
+
+@app.route("/scale-up", methods=['POST'])
+def scale_up():
+    data = request.get_json()
+
     config.load_kube_config()
-    pod = client.V1Pod(
-    metadata=client.V1ObjectMeta(name="my-python-pod"),
-    spec=client.V1PodSpec(
-        containers=[
-            client.V1Container(
-                name="my-python-container",
-                image="nginx",  # Example image
-                ports=[client.V1ContainerPort(container_port=80)],
-            )
-        ]
-    ),
-    )
+    v1 = client.AppsV1Api()
     try:
-        response = client.CoreV1Api().create_namespaced_pod(
-            body=pod, namespace="namespace-example"
-        )
-    except client.exceptions.ApiException as e:
-        print(f"An error occurred while creating a pod: {e}")
+        deployment = v1.read_namespaced_deployment(name=data.get("deployment-name"), namespace=data.get("deployment-namespace"))
+        deployment.spec.replicas = int(data.get("replicas"))
+        update = v1.patch_namespaced_deployment(name=data.get("deployment-name"), namespace=data.get("deployment-namespace"), body=deployment)
 
-def monitorar_pods():
+        return Response(status=200)
+    
+    except client.exceptions.ApiException as e:
+        return Response("Erro ao escalonar deploy", status=500)
+    
+@app.route("/scale-down", methods=['POST'])
+def scale_down():
+    data = request.get_json()
+
+    config.load_kube_config()
+    v1 = client.AppsV1Api()
+    try:
+        deployment = v1.read_namespaced_deployment(name=data.get("deployment-name"), namespace=data.get("deployment-namespace"))
+        deployment.spec.replicas = 1
+        update = v1.patch_namespaced_deployment(name=data.get("deployment-name"), namespace=data.get("deployment-namespace"), body=deployment)
+
+        return Response(status=200)
+    
+    except client.exceptions.ApiException as e:
+        return Response("Erro ao diminuir replicas", status=500)
+
+@app.route("/status", methods=['GET'])
+def monitor_pods():
     """Funçao para monitorar os pods de um namespace"""
+    data = request.get_json()
+
     config.load_kube_config()
     v1 = client.CoreV1Api()
     w = watch.Watch()
-    namespace = 'namespace-example'
-    print(f"Monitorando os pods: {namespace}")
+    namespace = data.get('namespace')
+
+    pod_list = []
     
     try:
         for event in w.stream(v1.list_namespaced_pod, namespace=namespace, timeout_seconds=60):
@@ -69,8 +113,9 @@ def monitorar_pods():
             print(f"Event: {event_type} - Pod: {pod_name} - Status: {pod_status}")
     except Exception as e:
         print(f"Error monitoring pods: {e}")
-        
-def monitorar_recursos_dos_pods():
+
+
+def monitor_pods_resources():
     """Funçao para monitorar os recursos dos pods"""
     #Tem que ativar : minikube addons enable metrics-server
     config.load_kube_config()
@@ -91,8 +136,9 @@ def monitorar_recursos_dos_pods():
 
 
 if __name__ == "__main__":
-    #get_minikube_pod_info()
+    # get_minikube_pod_info()
     # create_new_pod()
     # get_minikube_pod_info()
     #monitorar_pods()
-    monitorar_recursos_dos_pods()
+    # monitor_pods_resources()
+    app.run(debug=True)
